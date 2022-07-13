b9f2bf246c6b5c52034b31e7247dee2f3e3f272b
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-07-13 16:16:00 +0800

    Long way to make toggle button work - using add and remove

--- app.js
+++ app.js
@@ -3,3 +3,24 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector('.nav-toggle');
+const links =document.querySelector('.links');
+
+
+navToggle.addEventListener('click', function(){
+    // console.log(links.classList);
+    // console.log(links.classList.contains('random'));
+    // console.log(links.classList.contains('links'));
+ 
+    Long way - using add and remove
+    if(links.classList.contains('show-links')){
+        links.classList.remove('show-links');
+    }
+    else{
+        links.classList.add('show-links')
+    }
+ 
+
+})
+
